833de28b1cc9bd0f006343e1a1bed8b871d9876a
Romdra <roman_dra@mail.ru>
2021-02-28 12:36:16 +0500

    feat: task count-cats->completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+/* const CustomError = require("../extensions/custom-error"); */
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  const trueArr = matrix.filter(Boolean);  
+  if (trueArr.flat().includes('^^')) {
+    let arr = trueArr.flat().filter(el => el == '^^')
+    return arr.length;
+  } else {
+    return 0;
+  };
 };
+
+
+
+
