701a4580e071eecc80c6ff4b638df0d20a3285ae
Romdra <roman_dra@mail.ru>
2021-03-05 16:30:33 +0500

    feat: task simple-chain->completed

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
+
+
+ const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      this.chain.push(`( ${value} )`)
+      return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ( position == '' || isNaN(+position) || this.chain[position] == undefined) {
+      this.chain = []
+      throw new Error()
+    }
+    else {
+    this.chain.splice(position - 1, 1)
+    return this
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const newChain = this.chain.join('~~')
+    this.chain = []
+    return newChain
   }
-};
+}; 
 
 module.exports = chainMaker;
