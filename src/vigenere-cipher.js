90d22c06f58a6492d6b517f023890e4f0a761996
Romdra <roman_dra@mail.ru>
2021-03-08 20:35:45 +0500

    feat: task cipher->completed

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,82 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor (mode = true){
+    this.mode = mode
   }
+
+  encrypt(txt, key) {
+    const a =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+
+    txt = txt.toUpperCase();
+    key = key.toUpperCase();
+
+    let arr = [];
+    let j = 0;
+  
+    for (let i = 0; i < txt.length; i++) {
+      if (a.indexOf(txt[i]) == -1) {
+        arr.push(txt[i]);
+        continue;
+      }
+
+      let k = a[(a.indexOf(txt[i]) + a.indexOf(key[j])) % 26]
+      j++
+
+       if (j == key.length) {
+        j = 0;
+      } 
+
+      arr.push(k);
+    }
+
+    if(!this.mode) {
+      arr.reverse();
+    }
+
+    return arr.join('');
+
+  }
+
+  decrypt(txt, key) {
+    const a =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+
+    txt = txt.toUpperCase();
+    key = key.toUpperCase();
+
+    let arr = [];
+    let j = 0;
+  
+    for (let i = 0; i < txt.length; i++) {
+      if (a.indexOf(txt[i]) == -1) {
+        arr.push(txt[i]);
+        continue;
+      }
+
+    let res = a.indexOf(txt[i]) - a.indexOf(key[j]);
+    if ( res < 0 ) { res += 26 }
+    
+    let k = a[res];
+    j++
+    if (j == key.length) {
+      j = 0;
+    } 
+
+    arr.push(k);
+  }
+
+  if(!this.mode) {
+    arr.reverse();
+  }
+
+  return arr.join('');
+
+ }
 }
 
 module.exports = VigenereCipheringMachine;
+
+
+
+
